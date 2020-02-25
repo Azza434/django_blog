3df6b54d33ae057ce9a215d0cd0dd33e3f11acec
Azza434 <adwyer434@gmail.com>
2020-02-25 15:30:05 +0000

    Added Heroku and Postgres support for deployment

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+
+os.environ.setdefault("SECRET_KEY", '%slwy1ei@2k_==_juuo!0vzf(86+qt34l1_vli*89=9r5q3zv4')
+
+os.environ.setdefault("DATABASE_URL", 'postgres://qrxffswgnrvxvo:2251a6b28db6518ff9c6f2cbaf97ad3343021b28db42743e159bdbe5735c8f6b@ec2-46-137-84-173.eu-west-1.compute.amazonaws.com:5432/d4on9ggtcm6kur')
